b7270715203773df4c4876ff86ac722748a8a617
Kyle Maben <kylemaben@gmail.com>
2025-08-08 02:18:20 -0700

    fixed earlier issues and added testing

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,26 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// This is the essential line to enable controllers
+builder.Services.AddControllers();
+// These two lines add support for Swagger/OpenAPI, which is great for testing your endpoints
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// In a development environment, we want to use Swagger for easy testing.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-
-//public class books
-{
-   // public int Id { get; set; }
-   // public string? Name { get; set; }
-    //public bool IsComplete { get; set; }
-}
-
+// This is the essential line that maps the controller routes to the application.
+// Without this, the application won't know how to handle requests to your controllers.
+app.MapControllers();
 
 app.Run();
